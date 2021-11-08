3d68d1586acd0d0d89913d13ec971ee0b3af3458
MechEngMalik <mecheng.malik93@gmail.com>
2021-11-08 21:57:43 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Index from "./pages/Index";
+import Sidebar from "./components/Sidebar";
 import SignUp from "./Register/SignUp";
 import "./styles/App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -9,6 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/action" element={<Sidebar />} />
+
+
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Session.js
+++ src/pages/Session.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import axios from 'axios';
+import React, { Component } from 'react'
+import "/home/malek/project-401/FitFat-FrontEnd/src/styles/Sidebar.css"
+import {Button,Card} from 'react-bootstrap/';
 
-function Session() {
-  return (
-    <div className='session'>
-      <h1>session</h1>
-    </div>
-  );
+
+export class Session extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    sessionInfo:[],
+      
+
+    }
+  }
+
+  componentDidMount = async()=>{
+    let sessionFromApi = await axios.get(`http://localhost:8080/session`)
+
+    console.log(sessionFromApi.data);
+     this.setState({
+        sessionInfo: sessionFromApi.data
+      })
+  }
+  render() {
+ 
+
+    
+    return(
+        <div>
+
+        <Card style={{ width: '25rem' }} >
+          <Card.Body>
+            <Card.Title>Session name</Card.Title>
+            <Card.Text>
+              Description
+            </Card.Text>
+            <Card.Text>
+            Price
+            </Card.Text>
+            
+          </Card.Body>
+        <Button variant="outline-warning" size="lg" onClick={this.registerFunction}>
+              Register
+            </Button> 
+           
+           
+        </Card>
+
+        </div>
+      )
+  }
 }
 
-export default Session;
+export default Session
