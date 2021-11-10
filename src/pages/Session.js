f1e0d16dbf637c4c153fb2690e2ee0fd71b9c21e
MechEngMalik <mecheng.malik93@gmail.com>
2021-11-10 22:11:49 +0200

    finish-action-page

--- src/pages/Session.js
+++ src/pages/Session.js
@@ -30,25 +30,36 @@ export class Session extends Component {
       <div>
         <div class="container-fluid">
           <div class="row flex-nowrap">
-            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-              <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+          <div class="col-auto col-md-3 col-xl-2 px-sm-0 px-0 " id="lala">
+              
+              <div class="d-flex flex-column align-items-center align-items-sm-start   text-white min-vh-100 ">
 
-                <section className="home">
-
-                <Link to="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                  <span class="fs-5 d-none d-sm-inline accent-1"><span><FaHome/> </span> Home</span> 
+                
+                <div style={{backgroundColor:"#20252594" ,width:"100%", textAlign:"right"}}  class="flex-nowrap col-auto col-md-3 col-xl-2 px-sm-2 px-5 align-items-center px-sm-5" >
+                <Link to="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none px-sm-4
+                 pt-3">
+                  <span class="label-1">FitFat</span> 
                 </Link>
 
-                </section>
+                </div>
+                
+
+              
                 <section className="container100">
 
 
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                  <li class="nav-item">
+                <li class="nav-item">
+                    <Link to="/" class="nav-link align-middle px-0">
+                      <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline"><span><FaHome/> </span>Home</span>
+                    </Link>
+                  </li>
+
+                  {/* <li class="nav-item">
                     <Link to="/user/profile" class="nav-link align-middle px-0">
                       <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline"><span><CgProfile/> </span>Profile</span>
                     </Link>
-                  </li>
+                  </li> */}
                   <li>
                     <Link to="/gym" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
                       <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline"><span><CgGym/></span>Gym List</span> </Link>
